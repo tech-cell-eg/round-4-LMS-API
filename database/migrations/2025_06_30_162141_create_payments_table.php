ced8b8290c3d4e6e73705c490efac3c5de00e9c9
awael <ahmed.wm05@gmail.com>
2025-07-01 10:39:38 +0300

    handel migrations

--- database/migrations/2025_06_30_162141_create_payments_table.php
+++ database/migrations/2025_06_30_162141_create_payments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cart_id')->constrained()->onDelete('cascade');
-            $table->foreignId('credit_card_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('credit_card_id')->nullable()->constrained('credit_cards')->onDelete('set null');
             $table->enum( 'payment_method', ['credit_card', 'paypal'])->default('credit_card');
             $table->float('amount', 8, 2);
             $table->string('country')->unique();
