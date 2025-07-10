fec77e4d57fe88b0c9f9d15a7992f56b37c7863c
Moustafa Saber <sabr72509@gmail.com>
2025-07-10 21:23:46 +0300

    X

--- routes/api.php
+++ routes/api.php
@@ -24,11 +24,11 @@ use App\Http\Controllers\Api\Instructor\CouponController;
     Route::post('logout', 'logout')->middleware('auth:sanctum');
 });
 
-    Route::get('/courses/{slug}', [InstructorCourseController::class, 'show']); // up
 // Instructor Routes
 Route::group(['middleware' => ['auth:sanctum', 'is_instructor']], function () {
     Route::post('/courses', [InstructorCourseController::class, 'store']);
-
+    Route::get('/courses/{slug}', [InstructorCourseController::class, 'show']); // up
+    
 
         // coupons
 
